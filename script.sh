b81b89a8aaf634cc90e2de04260dd2c88322618a
muse-sisay <git.muse.sisay@gmail.com>
2021-06-23 23:16:20 +0100

    feat(script.sh): set local git config
    Will add a local git config if the git_user passed is diffrent from the (users) global git config.
    Also the remote hostname is changed from github.com to github-{username}. This allows to easily
    manage public keys through ssh config.

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
-LICENSES=("agpl-3.0" "apache-2.0" "bsd-2-clause" "bsd-3-clause"\
-        "bsl-1.0" "cc0-1.0" "epl-2.0" "gpl-2.0" "gpl-3.0" "lgpl-2.1"\
+LICENSES=("agpl-3.0" "apache-2.0" "bsd-2-clause" "bsd-3-clause" \
+        "bsl-1.0" "cc0-1.0" "epl-2.0" "gpl-2.0" "gpl-3.0" "lgpl-2.1" \
          "mit" "mpl-2.0" "unlicense" )
 
 USAGE="Syntax: gitinit  [-s] [-u GH_USERNAME] [-p PROJECT TITLE] [-l LICENSE] [-d PATH] \"Repository-name\""
@@ -14,29 +14,48 @@ function usage()
 
 function help(){
 
-    echo "gitinit is tool for intialize a git repo and push an inital commit to github."
+    echo
+    echo "gitinit is tool for intialize a git repo and push an inital commit to Github."
     echo
     echo $USAGE
-    echo "options:"
-    echo "h     Print this Help."
-    echo "-u    USERNAME "
-    echo "-p    PROJECT TITLE  h1 displayed on README.md. Defaults to repository-name"
-    echo "s     set as a private repository."
+    echo
+    echo " options:"
+    echo "  - h    Print this Help."
+    echo "  - u    git USERNAME "
+    echo "  - p    PROJECT TITLE  heading displayed on README.md. Defaults to repository-name"
+    echo "  - s    set as a private repository."
+    echo
     exit 2
 
 }
 
-function set_username(){
+function read_global_git_config(){
     # Reads username from  git config 
-    user_name=$(awk '/name/ {print $3}' ~/.gitconfig)
+    global_git_user=$(awk '/name/ {print $3}' ~/.gitconfig 2> /dev/null)
+    
+    if [ "$?"  -eq 2 ] && [ -z "$git_user" ]; then 
+        echo "Missing global git config. Add using "
+        echo "    git config --global user.name"
+        echo "    git config --global user.email"
+        exit 
+    fi
 }
-function check_username(){
 
-    if [ -z "$user_name" ]; then 
-            echo "Missing username, can't find username in ~/.gitconfig"
-            usage
+function set_username(){
+   
+    read_global_git_config
+
+    if [ -z "$git_user" ]; then
+        git_user=$global_git_user
     fi
 
+    # Only create a local git config  
+    # if git_user is diffrent  global_git_user
+    if [ $git_user != $global_git_user ]; then
+        # Prompt for git email
+        read -p "Git email: " git_email
+        set_local_repo="true"
+    fi
 }
 
 function check_license(){ # ALT : check_valid_license
@@ -56,18 +75,15 @@ function directory_exists(){ # Check if directory exitsts
     fi
 }
 
-
-
 function setup_args(){
 
     # USERNAME
-    [ -z "$user_name" ] && set_username
-    check_username
+    set_username
 
     # API
     # (read) Access/api token
     api_token=$(cat ~/.ssh/ghAccessToken) # TODO : check if file exists
-    repo_name=${@: -1}
+    repo_name=$1
 
     #  PROJECT TITLE
     [ -z "$project_title" ] && project_title="$repo_name"
@@ -81,7 +97,7 @@ function create_local_repo_path(){
     
     cd "$loc"
 
-    if [ -d "$repo_name" ]; then
+    if [ -e "$repo_name" ]; then
         echo "$0: cannot create directory ‘${repo_name}’ in $loc : Project exists"
         exit 3
     fi 
@@ -91,7 +107,7 @@ function create_local_repo_path(){
 
 }
 
-function create_repo_files(){
+function create_project_files(){
 
     echo "# $project_title" > README.md
 
@@ -100,10 +116,29 @@ function create_repo_files(){
             https://api.github.com/licenses/$license | jq -r .body > LICENSE
     fi 
 }
-while getopts 'sl::u:p:d:h' args ;do 
+
+function intialize_repo(){
+    
+    git init
+
+    if [ ! -z $set_local_repo ]; then 
+        git config user.name $git_user
+        git config user.email $git_email
+    fi
+
+    git remote add origin git@"github-${git_user}":"$git_user"/"$repo_name".git
+
+    git add README.md LICENSE
+    git commit -m "intial commit"
+}
+
+# /////////////////////////////////////////////
+# ////////////////////////////////////////////
+
+while getopts 'sl:u:p:d:h' args "${@:1:$#-1}" ;do 
     case $args in 
         u) 
-            user_name=$OPTARG
+            git_user=$OPTARG
         ;;
         p) 
            project_title=$OPTARG
@@ -120,16 +155,17 @@ while getopts 'sl::u:p:d:h' args ;do
             private_repo=",\"private\" : true" #BUG : passing this without any arguments breaks the script
         ;;
         h |  *)
-            usage
+            help
         ;;
     esac
 done 
 
-setup_args
+setup_args ${@: -1}
 
 # LOCAL REPO
 create_local_repo_path
-create_repo_files
+create_project_files
+intialize_repo 
 
 # REMOTE REPO
 if [ $(curl -s -o /dev/null -w "%{http_code}" \
@@ -139,11 +175,5 @@ if [ $(curl -s -o /dev/null -w "%{http_code}" \
     echo "Unable to create remote repo."
     exit 5
 fi
-
-git init
-git remote add origin git@github.com:"$user_name"/"$repo_name".git
-
-git add README.md LICENSE
-git commit -m "intial commit"
 
 git push -u origin master
